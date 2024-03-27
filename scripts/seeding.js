929ecdbf2ff9a3b1e1095077f81ac81fb85e7cce
Edward Kim <itgma1231@gmail.com>
2024-03-27 18:24:14 -0400

    /scripts/seeding.js - 20 records added for seeding; package.json seed script

--- scripts/seeding.js
+++ scripts/seeding.js
@@ -18,11 +18,9 @@ async function main() {
     config[chainId].MedicalRecord.address
   );
 
-  console.log(
-    `Refetch MedicalRecord contract address: ${contract.address}`
-  );
+  console.log(`Refetch MedicalRecord contract address: ${contract.address}`);
 
-  // Add record
+  // Add records
   transaction = await contract
     .connect(signer[0])
     .addRecord(
@@ -34,9 +32,272 @@ async function main() {
       'Hypertension',
       'Medications'
     );
+  await transaction.wait();
+  console.log(`Record ID: ${await contract.getRecordId()} added.`);
 
-    await transaction.wait();
-    console.log(`Record ID: ${await contract.getRecordId()} added.`)
+  transaction = await contract
+    .connect(signer[0])
+    .addRecord(
+      'Jane Smith',
+      40,
+      'Female',
+      'A Negative',
+      'Pollen',
+      'Diabetes',
+      'Insulin'
+    );
+  await transaction.wait();
+  console.log(`Record ID: ${await contract.getRecordId()} added.`);
+
+  transaction = await contract
+    .connect(signer[0])
+    .addRecord(
+      'Michael Johnson',
+      55,
+      'Male',
+      'B Positive',
+      'Penicillin',
+      'Arthritis',
+      'Physical Therapy'
+    );
+  await transaction.wait();
+  console.log(`Record ID: ${await contract.getRecordId()} added.`);
+
+  transaction = await contract
+    .connect(signer[0])
+    .addRecord(
+      'Emily Johnson',
+      28,
+      'Female',
+      'AB Negative',
+      'Nuts',
+      'Migraine',
+      'Painkillers'
+    );
+  await transaction.wait();
+  console.log(`Record ID: ${await contract.getRecordId()} added.`);
+
+  transaction = await contract
+    .connect(signer[0])
+    .addRecord(
+      'David Williams',
+      50,
+      'Male',
+      'O Negative',
+      'Shellfish',
+      'High Cholesterol',
+      'Diet and Exercise'
+    );
+  await transaction.wait();
+  console.log(`Record ID: ${await contract.getRecordId()} added.`);
+
+  transaction = await contract
+    .connect(signer[0])
+    .addRecord(
+      'Sarah Lee',
+      35,
+      'Female',
+      'A Positive',
+      'Dairy',
+      'Asthma',
+      'Inhaler'
+    );
+  await transaction.wait();
+  console.log(`Record ID: ${await contract.getRecordId()} added.`);
+
+  transaction = await contract
+    .connect(signer[0])
+    .addRecord(
+      'Christopher Brown',
+      45,
+      'Male',
+      'B Positive',
+      'Eggs',
+      'Depression',
+      'Therapy'
+    );
+  await transaction.wait();
+  console.log(`Record ID: ${await contract.getRecordId()} added.`);
+
+  transaction = await contract
+    .connect(signer[0])
+    .addRecord(
+      'Jessica Martinez',
+      32,
+      'Female',
+      'O Negative',
+      'Peanuts',
+      'Anxiety',
+      'Counseling'
+    );
+  await transaction.wait();
+  console.log(`Record ID: ${await contract.getRecordId()} added.`);
+
+  transaction = await contract
+    .connect(signer[0])
+    .addRecord(
+      'Daniel Wilson',
+      55,
+      'Male',
+      'A Negative',
+      'Seafood',
+      'Hypothyroidism',
+      'Medication'
+    );
+
+  transaction = await contract
+    .connect(signer[0])
+    .addRecord(
+      'Michelle Anderson',
+      38,
+      'Female',
+      'AB Positive',
+      'Gluten',
+      'Fibromyalgia',
+      'Physical Therapy'
+    );
+  await transaction.wait();
+  console.log(`Record ID: ${await contract.getRecordId()} added.`);
+
+  transaction = await contract
+    .connect(signer[0])
+    .addRecord(
+      'Kevin Taylor',
+      42,
+      'Male',
+      'O Positive',
+      'None',
+      'High Blood Pressure',
+      'Medication'
+    );
+
+  transaction = await contract
+    .connect(signer[0])
+    .addRecord(
+      'Linda Harris',
+      60,
+      'Female',
+      'B Negative',
+      'Shellfish',
+      'Osteoporosis',
+      'Calcium Supplements'
+    );
+  await transaction.wait();
+  console.log(`Record ID: ${await contract.getRecordId()} added.`);
+
+  transaction = await contract
+    .connect(signer[0])
+    .addRecord(
+      'Jason Clark',
+      28,
+      'Male',
+      'A Positive',
+      'Pollen',
+      'Seasonal Allergies',
+      'Antihistamines'
+    );
+  await transaction.wait();
+  console.log(`Record ID: ${await contract.getRecordId()} added.`);
+
+  transaction = await contract
+    .connect(signer[0])
+    .addRecord(
+      'Kimberly Ramirez',
+      50,
+      'Female',
+      'O Negative',
+      'Dairy',
+      'Osteoarthritis',
+      'Physical Therapy'
+    );
+  await transaction.wait();
+  console.log(`Record ID: ${await contract.getRecordId()} added.`);
+
+  transaction = await contract
+    .connect(signer[0])
+    .addRecord(
+      'Ryan Cooper',
+      35,
+      'Male',
+      'AB Positive',
+      'None',
+      'GERD',
+      'Lifestyle Changes'
+    );
+  await transaction.wait();
+  console.log(`Record ID: ${await contract.getRecordId()} added.`);
+
+  transaction = await contract
+    .connect(signer[0])
+    .addRecord(
+      'Erica Thompson',
+      45,
+      'Female',
+      'A Positive',
+      'None',
+      'Type 2 Diabetes',
+      'Insulin Therapy'
+    );
+  await transaction.wait();
+  console.log(`Record ID: ${await contract.getRecordId()} added.`);
+
+  transaction = await contract
+    .connect(signer[0])
+    .addRecord(
+      'Matthew Hill',
+      55,
+      'Male',
+      'B Positive',
+      'Pollen',
+      'COPD',
+      'Bronchodilators'
+    );
+  await transaction.wait();
+  console.log(`Record ID: ${await contract.getRecordId()} added.`);
+
+  transaction = await contract
+    .connect(signer[0])
+    .addRecord(
+      'Stephanie Barnes',
+      30,
+      'Female',
+      'O Positive',
+      'Peanuts',
+      'Atrial Fibrillation',
+      'Blood Thinners'
+    );
+  await transaction.wait();
+  console.log(`Record ID: ${await contract.getRecordId()} added.`);
+
+  transaction = await contract
+    .connect(signer[0])
+    .addRecord(
+      'Mark Evans',
+      48,
+      'Male',
+      'AB Negative',
+      'Shellfish',
+      "Crohn's Disease",
+      'Immunosuppressants'
+    );
+  await transaction.wait();
+  console.log(`Record ID: ${await contract.getRecordId()} added.`);
+
+  transaction = await contract
+    .connect(signer[0])
+    .addRecord(
+      'Emily White',
+      33,
+      'Female',
+      'A Negative',
+      'Shellfish',
+      'Rheumatoid Arthritis',
+      'Immunosuppressants'
+    );
+  await transaction.wait();
+  console.log(`Record ID: ${await contract.getRecordId()} added.`);
+
+  console.log('ALL RECORDS ADDED.');
 }
 
 main().catch((error) => {
